95f27e17246986eaa00622d1d69d0579bb59f752
Jeffrey Deng <jdeng92@gmail.com>
2014-03-23 15:42:23 -0700

    Creating EventsList singleton

---
+++ MindYourPills/EventsList.m
@@ -0,0 +1,30 @@
+//
+//  EventsList.m
+//  MindYourPills
+//
+//  Created by Jeffrey Deng on 3/23/14.
+//  Copyright (c) 2014 Sarah Wada. All rights reserved.
+//
+
+#import "EventsList.h"
+
+@implementation EventsList
+
+static NSMutableArray *sharedEventListInstance = nil;
+
++ (NSMutableArray *)sharedEventListInstance {
+ 
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedEventListInstance = [[self alloc] init];
+    });
+    
+    return sharedEventListInstance;
+}
+
+- (id) init {
+    self = [super init];
+    return self;
+}
+
+@end
