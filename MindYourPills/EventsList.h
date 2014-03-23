b0a93f188561b73c644f14f6f51a0536a8dc9322
Jeffrey Deng <jdeng92@gmail.com>
2014-03-23 15:42:23 -0700

    Creating EventsList singleton

---
+++ MindYourPills/EventsList.h
@@ -0,0 +1,17 @@
+//
+//  EventsList.h
+//  MindYourPills
+//
+//  Created by Jeffrey Deng on 3/23/14.
+//  Copyright (c) 2014 Sarah Wada. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EventsList : NSObject
+
++ (NSMutableArray *)sharedEventListInstance;
+
+
+
+@end
