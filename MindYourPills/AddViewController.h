08056b284b2aedec0ecb289984fd62b26696b756
Yujun Cho <yujuncho@Yujuns-MacBook-Pro.local>
2014-03-23 13:22:19 -0700

    Adding view controllers for home page, add page, and edit page

---
+++ MindYourPills/AddViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddViewController.h
+//  MindYourPills
+//
+//  Created by Yujun Cho on 3/23/14.
+//  Copyright (c) 2014 Sarah Wada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddViewController : UIViewController
+
+@end
---
+++ MindYourPills/AddViewController.m
@@ -0,0 +1,34 @@
+//
+//  AddViewController.m
+//  MindYourPills
+//
+//  Created by Yujun Cho on 3/23/14.
+//  Copyright (c) 2014 Sarah Wada. All rights reserved.
+//
+
+#import "AddViewController.h"
+
+@interface AddViewController ()
+
+@property (nonatomic, weak) IBOutlet UITextField *nameTextField;
+@property (nonatomic, weak) IBOutlet UITextField *reasonTextField;
+@property (nonatomic, weak) IBOutlet UITextField *amountTextField;
+@property (nonatomic, weak) IBOutlet UITextField *frequencyTextField;
+@property (nonatomic, weak) IBOutlet UIPickerView *formPickerView;
+@property (nonatomic, weak) IBOutlet UIDatePicker *timeDatePicker;
+
+@end
+
+@implementation AddViewController
+
+- (IBAction)save:(id)sender
+{
+    
+}
+
+- (IBAction)exit:(id)sender
+{
+    
+}
+
+@end
