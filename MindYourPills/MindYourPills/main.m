379af8a1b5045ff3fbaac39dff1e4e3452270619
Sarah Wada <sarahwada@Sarahs-MacBook-Pro-2.local>
2014-03-22 17:38:32 -0700

    empty ios app, MindYourPills

---
+++ MindYourPills/MindYourPills/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MindYourPills
+//
+//  Created by Sarah Wada on 3/22/14.
+//  Copyright (c) 2014 Sarah Wada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ MindYourPills/MindYourPillsTests/MindYourPillsTests.m
@@ -0,0 +1,34 @@
+//
+//  MindYourPillsTests.m
+//  MindYourPillsTests
+//
+//  Created by Sarah Wada on 3/22/14.
+//  Copyright (c) 2014 Sarah Wada. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MindYourPillsTests : XCTestCase
+
+@end
+
+@implementation MindYourPillsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
