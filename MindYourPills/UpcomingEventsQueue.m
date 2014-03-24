5364e491d5a7e794281e07c8943f148c74200f46
Jeffrey Deng <jdeng92@gmail.com>
2014-03-23 17:20:05 -0700

    created files needed for our queue

---
+++ MindYourPills/UpcomingEventsQueue.m
@@ -0,0 +1,13 @@
+//
+//  UpcomingEventsQueue.m
+//  MindYourPills
+//
+//  Created by Jeffrey Deng on 3/23/14.
+//  Copyright (c) 2014 Sarah Wada. All rights reserved.
+//
+
+#import "UpcomingEventsQueue.h"
+
+@implementation UpcomingEventsQueue
+
+@end
