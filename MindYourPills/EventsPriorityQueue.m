b6dd699c66ed8a1b456a97d98d32bbdfbee9fbc5
Jeffrey Deng <jdeng92@gmail.com>
2014-03-23 17:20:05 -0700

    merging with jeff branch

---
+++ MindYourPills/EventsPriorityQueue.m
@@ -0,0 +1,13 @@
+//
+//  EventsPriorityQueue.m
+//  MindYourPills
+//
+//  Created by Jeffrey Deng on 3/23/14.
+//  Copyright (c) 2014 Sarah Wada. All rights reserved.
+//
+
+#import "EventsPriorityQueue.h"
+
+@implementation EventsPriorityQueue
+
+@end
---
+++ MindYourPills/UpcomingEventsQueue.h
@@ -0,0 +1,13 @@
+//
+//  UpcomingEventsQueue.h
+//  MindYourPills
+//
+//  Created by Jeffrey Deng on 3/23/14.
+//  Copyright (c) 2014 Sarah Wada. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UpcomingEventsQueue : NSObject
+
+@end
