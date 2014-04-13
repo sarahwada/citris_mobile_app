415e7d8886cd20d0bba0392be4fdfb8875ee6047
Yujun <yujuncho7@gmail.com>
2014-04-12 19:28:39 -0700

    lost scheduleTimesViews for some reason. Readded them

---
+++ MindYourPills/scheduleTimesViewController.h
@@ -0,0 +1,13 @@
+//
+//  scheduleTimesViewController.h
+//  MindYourPills
+//
+//  Created by Yujun Cho on 4/6/14.
+//  Copyright (c) 2014 Sarah Wada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface scheduleTimesViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+@end
